ff7a036b3e43413f8bf85b11a6537598c49fa95f
MackyClap <martimac2320@gmail.com>
2022-08-16 18:55:37 -0400

    defined assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,33 @@
+const eqArrays = function(arrOne, arrTwo) {
+  let ans = Boolean(0);
+  for (let x = 0; x < arrOne.length; x++) {
+    if (arrOne[x] === arrTwo[x]) {
+      ans = Boolean(1);
+    } else {
+      ans = Boolean(0);
+    }
+  }
+  return ans;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  let ans = Boolean(0);
+  for (let x = 0; x < arrOne.length; x++) {
+    if (arrOne[x] === arrTwo[x]) {
+      ans = Boolean(1);
+    } else {
+      ans = Boolean(0);
+    }
+  }
+  if (ans !== Boolean(0)) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
