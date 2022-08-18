72e87ab959a85049cab52aa6a46b1e6d534a1e3c
MackyClap <martimac2320@gmail.com>
2022-08-17 22:29:29 -0400

    updated code logic as it broke when working on without.js

--- eqArrays.js
+++ eqArrays.js
@@ -6,20 +6,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arrOne, arrTwo) {
-  let ans = Boolean(0);
-  for (let x = 0; x < arrOne.length; x++) {
-    if (arrOne[x] === arrTwo[x]) {
-      ans = Boolean(1);
-    } else {
-      ans = Boolean(0);
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let x = 0; x < arr1.length; x++) {
+    if (arr1[x] !== arr2[x]) {
+      
+      return false;
     }
   }
-  return ans;
+  return true;
 };
+
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays([1, 2, 3], [3, 2, 3]); // => true
+
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
@@ -27,4 +31,4 @@ eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 3, 3]), false);
