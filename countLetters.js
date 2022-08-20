711f6a9ccef2f918230b4497ef72022c73503845
MackyClap <martimac2320@gmail.com>
2022-08-20 17:36:22 -0400

    Completed countLetters.js code

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let ansCounter = {};
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (ansCounter[letter]) {
+        ansCounter[letter] += 1;
+      } else {
+        ansCounter[letter] = 1;
+      }
+    }
+  }
+  console.log(ansCounter);
+  return ansCounter;
+};
+
+countLetters("Macky Martinez");
