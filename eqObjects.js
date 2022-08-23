46fac57c739c7fd646bd4d254000f5edbaec33bb
MackyClap <martimac2320@gmail.com>
2022-08-22 20:38:36 -0400

    Completed Step 4 testing for if value is an array and then comparing if they are

--- eqObjects.js
+++ eqObjects.js
@@ -23,30 +23,33 @@ const eqArrays = function(arr1,arr2) {
 
 
 //function definition
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   const keyObj1 = Object.keys(object1);
   const keyObj2 = Object.keys(object2);
-    if(keyObj1.length !== keyObj2.length) {
-      return false;
-    } else {
-      for (const keys1 of keyObj1){
-        // console.log(object2[keys1]);
-        if(object1[keys1] !== object2[keys1]){
-          // console.log("object1 keys:", object1[keys1]);
-          // console.log("object2 keys:", object2[keys1]);
+  if (keyObj1.length !== keyObj2.length) {
+    return false;
+  } else {
+    for (const keys1 of keyObj1) {
+      // console.log(object2[keys1]);
+      if (Array.isArray(object1[keys1]) && Array.isArray(object2[keys1])) {
+        const arrayEql = eqArrays(object1[keys1],object2[keys1]);
+        if (arrayEql === false) {
           return false;
-         } 
         }
-        return true;
+      }
+      else if (object1[keys1] !== object2[keys1]) {
+        return false;
+      }
     }
-  };
+    return true;
+  }
+};
   
 
 
 
 // Asertion code - arrays in objects
+
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 console.log("eqObjects with arrays: ", eqObjects(cd, dc)); // => true
@@ -56,10 +59,10 @@ console.log("eqObjects with arrays: ", eqObjects(cd, cd2)); // => false
 assertEqual(eqObjects(cd, cd2), false);
 
 // //Assertion code - Primitive values
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
 
-// console.log("eqObjects: ", eqObjects(ab, ba)); // => true
+console.log("eqObjects: ", eqObjects(ab, ba)); // => true
 
-// const abc = { a: "1", b: "2", c: "3" };
-// console.log("eqObjects: ", eqObjects(ab, abc)); // => false
+const abc = { a: "1", b: "2", c: "3" };
+console.log("eqObjects: ", eqObjects(ab, abc)); // => false
