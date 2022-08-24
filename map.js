439393d8f214e10604712c8b890608e53a6e4012
MackyClap <martimac2320@gmail.com>
2022-08-24 19:11:44 -0400

    Completed map.js implementation and tested with eqarrays

---
+++ map.js
@@ -0,0 +1,43 @@
+//Assert codes
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let x = 0; x < arr1.length; x++) {
+    if (arr1[x] !== arr2[x]) {
+      
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo) === true) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+};
+
+//implement map
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+
+console.log(results1);
+
+//test assertions
+
+assertArraysEqual(results1, ['g','c','t','m','t']); //true
+assertArraysEqual(results1, ['g','c','t','m','k']); //false
+assertArraysEqual(results1, []); //false
