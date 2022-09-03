9775ee3eeb8192fcd45b7cde0f8d1c88396fa802
MackyClap <martimac2320@gmail.com>
2022-09-03 19:48:03 -0400

    refactor letterPositionsTest to us mocha and chai

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,9 +1,13 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert
 const letterPositions = require('../letterPositions')
-//ASSERT CODE
 
-assertArraysEqual(letterPositions("Hi Macky, Macky").M, [3,10]); // False
-assertArraysEqual(hAns['H'], [0]); // True
-assertArraysEqual(hAns['H'], [0,2]); // False
-assertArraysEqual(hAns, {H: [ 0 ], i: [ 1 ], M: [ 3 ],a: [ 4 ], c: [ 5 ],k: [ 6 ],y: [ 7 ]}); // True
+//ASSERT CODE
+describe("#letterPositions", () => {
+  it("returns false for letterPositions(Hi Macky, Macky).M, [3,10]", () => {
+    assert.deepEqual(letterPositions("Hi Macky, Macky").M, [3,10], false);
+  });
+});
+
+// assertArraysEqual(hAns['H'], [0]); // True
+// assertArraysEqual(hAns['H'], [0,2]); // False
+// assertArraysEqual(hAns, {H: [ 0 ], i: [ 1 ], M: [ 3 ],a: [ 4 ], c: [ 5 ],k: [ 6 ],y: [ 7 ]}); // True
